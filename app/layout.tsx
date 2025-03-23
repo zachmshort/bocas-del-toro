511608ce1da0650899cb535f0938adc6f846a460
zachmshort <zach.short>
2025-03-23 14:37:25 -0400

    unusedg

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Pacifico, Poppins, Montserrat } from "next/font/google";
-
-const pacifico = Pacifico({
-  weight: "400",
-  subsets: ["latin"],
-  display: "swap",
-});
-
-const poppins = Poppins({
-  weight: ["400", "600"],
-  subsets: ["latin"],
-  display: "swap",
-});
+import { Montserrat } from "next/font/google";
 
 const montserrat = Montserrat({
   weight: ["400", "500", "700"],
